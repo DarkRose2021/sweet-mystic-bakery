aeccb948c4aabfabc4e9296f6e60efa8a8c0c8ca
Katie King <89149647+DarkRose2021@users.noreply.github.com>
2026-07-22 23:18:45 -0500

    mobile vercel

--- src/pages/Launch/Launch.js
+++ src/pages/Launch/Launch.js
@@ -1,7 +1,7 @@
 import "./Launch.scss";
 
-import logo from "../../assets/images/headerimg.png";
-import bakeryImage from "../../assets/images/Main-Logo.png";
+import logo from "../../assets/images/Main-Logo.png";
+import bakeryImage from "../../assets/images/temp.jpg";
 
 function Launch() {
 	return (
